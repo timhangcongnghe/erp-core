48e0a107f158f7fbd84b59c88670c7bce19cd926
Hung Nguyen <hungnt@hoangkhang.com.vn>
2016-12-16 18:54:39 -0800

    add reference columns creator for erp_users

--- db/migrate/20161212042313_add_columns_to_erp_users.rb
+++ db/migrate/20161212042313_add_columns_to_erp_users.rb
@@ -3,5 +3,6 @@ class AddColumnsToErpUsers < ActiveRecord::Migration[5.0]
     add_column :erp_users, :name, :string
     add_column :erp_users, :timezone, :string
     add_column :erp_users, :active, :boolean, default: false
+    add_reference :erp_users, :creator, index: true, references: :erp_users
   end
 end
