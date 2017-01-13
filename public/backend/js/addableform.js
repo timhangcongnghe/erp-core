dd7cf0bf8466de5abfbad6324dd49eda021c936b
Luan Pham <luanpm88@gmail.com>
2017-01-13 01:25:18 -0800

    addabletable

--- public/backend/js/addableform.js
+++ public/backend/js/addableform.js
@@ -2,14 +2,31 @@ function addableformAddLine(addableform) {
     var url = addableform.attr('partial-url');
     var partial = addableform.attr('partial');
     var container = addableform.find('.addableform-container');
+    var type = addableform.attr('type');
+    var add_control = $(addableform.attr('add-control-selector'));
+    
+    if(typeof(add_control) === 'undefined') {
+        add_value = '';
+    } else {
+        add_value = add_control.val();
+    }
+    
+    if(typeof(type) === 'undefined') {
+        type = 'normal';
+    }
     
     $.ajax({
         url: url,
         data: {
-            partial: partial
+            partial: partial,
+            add_value: add_value
         }
     }).done(function( result ) {
-        container.append('<span class="addableform-line">' + result + '</span>');
+        if(type !== 'table') {
+            container.append('<span class="addableform-line">' + result + '</span>');
+        } else {
+            container.append('<tr class="addableform-line">' + result + '</tr>');
+        }
         
         // js for new content
         jsForAjaxContent(container.find('.addableform-line').last());
