92e2b4df68f56c669cb86cab28accf999d45342b
sonnguyenngoc <sonnn@hoangkhang.com.vn>
2017-02-09 22:23:28 -0800

    format currency

--- app/helpers/erp/application_helper.rb
+++ app/helpers/erp/application_helper.rb
@@ -84,5 +84,21 @@ module Erp
     def export_partial
       "erp/elements/export.xlsx.axlsx"
     end
+    
+    def format_number(number, vn = false, round = false, precision = nil)
+      prec = (number.to_f.round == number.to_f) ? 0 : 2
+      prec = 0 if round
+      
+      if !precision.nil?
+        prec = precision
+      end
+    
+      if vn
+        number_to_currency(number, precision: prec, separator: ",", unit: '', delimiter: ".")
+      else
+        number_to_currency(number, precision: prec, separator: ".", unit: '', delimiter: ",")
+      end
+    end
+    
   end
 end
