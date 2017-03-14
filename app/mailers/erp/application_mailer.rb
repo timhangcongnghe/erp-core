e9132b6b2b6700a235600e2a6e843111e312ebf1
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-03-13 19:37:14 -0700

    setup/configure mailer

---
+++ app/mailers/erp/application_mailer.rb
@@ -0,0 +1,20 @@
+module Erp
+  class ApplicationMailer < ActionMailer::Base
+    default from: "soft.support@hoangkhang.com.vn"
+    layout 'mailer'
+    
+    private
+    def send_email(email, subject) 
+      #@todo static email!!
+      delivery_options = {address: 'smtp.gmail.com',
+                          port: 587,
+                          domain: 'globalnaturesoft.com',
+                          user_name: 'soft.support@hoangkhang.com.vn',
+                          password: 'aA456321@#$',
+                          authentication: 'plain'}
+      mail(to: email,
+           subject: subject,
+           delivery_method_options: delivery_options)
+    end
+  end
+end
