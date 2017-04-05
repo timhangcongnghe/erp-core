0e7fe9aae40484a13665e82c938b0b41b1c3bde3
Luan Pham <luanpm88@gmail.com>
2017-04-05 00:36:11 -0700

    update some core features

--- app/controllers/erp/application_controller.rb
+++ app/controllers/erp/application_controller.rb
@@ -5,14 +5,14 @@ module Erp
 		before_action :set_cart
 		include Erp::Carts::Frontend::Concerns::CurrentCompare
 		before_action :set_compare
-		
+
 		Dir.glob(Rails.root.join('engines').to_s + "/*") do |d|
 			eg = d.split(/[\/\\]/).last
 			if eg != "core" and Erp::Core.available?(eg)
 				helper "Erp::#{eg.camelize}::Engine".constantize.helpers
 			end
 		end
-		
+
 		# @todo: seperate backend fronend api
 		def after_sign_in_path_for(resource_or_scope)
 			if session[:current_view] == 'frontend'
--- app/mailers/erp/application_mailer.rb
+++ app/mailers/erp/application_mailer.rb
@@ -2,16 +2,17 @@ module Erp
   class ApplicationMailer < ActionMailer::Base
     default from: "soft.support@hoangkhang.com.vn"
     layout 'mailer'
-    
+
     private
-    def send_email(email, subject) 
+    def send_email(email, subject)
       #@todo static email!!
       delivery_options = {address: 'smtp.gmail.com',
                           port: 587,
                           domain: 'globalnaturesoft.com',
                           user_name: 'soft.support@hoangkhang.com.vn',
                           password: 'aA456321@#$',
-                          authentication: 'plain'}
+                          authentication: 'plain',
+                          enable_starttls_auto: true}
       mail(to: email,
            subject: subject,
            delivery_method_options: delivery_options)
