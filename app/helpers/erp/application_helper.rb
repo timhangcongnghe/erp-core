850ecc7b97844e6c9b47a50f29ef25d51486c555
Luan Pham <luanpm88@gmail.com>
2017-04-10 02:54:52 -0700

    update layouts

--- app/helpers/erp/application_helper.rb
+++ app/helpers/erp/application_helper.rb
@@ -4,37 +4,37 @@ module Erp
     def erp_form_control(type, options={})
       render partial: "erp/helpers/form_controls/#{type}", locals: { options: options }
     end
-    
+
     # components
     def erp_component(type, options={})
       render partial: "erp/helpers/components/#{type}", locals: { options: options }
     end
-    
+
     # list actions button for list
     def erp_datalist_list_actions(actions=[])
       render partial: "erp/helpers/list/datalist_list_actions", locals: { actions: actions }
     end
-    
+
     # list actions button for each row of list
     def erp_datalist_row_actions(actions=[])
       render partial: "erp/helpers/list/datalist_row_actions", locals: { actions: actions }
     end
-    
+
     # filters button for list
     def erp_datalist_filters(actions=[])
       render partial: "erp/helpers/list/datalist_filters", locals: { actions: actions }
     end
-    
+
     # columns button for list
     def erp_datalist_columns_select(actions=[])
       render partial: "erp/helpers/list/datalist_columns_select", locals: { actions: actions }
     end
-    
+
     # Datalist main helper
     def erp_datalist(options=[])
       render partial: "erp/helpers/list/datalist", locals: { options: options }
     end
-    
+
     # Datalist check all checkbox
     def erp_datalist_check_all
       '<label class="mt-checkbox mt-checkbox-single mt-checkbox-outline">
@@ -42,7 +42,7 @@ module Erp
           <span></span>
       </label>'.html_safe
     end
-    
+
     # Datalist check all checkbox
     def erp_datalist_check_row(options={})
       ('<label class="mt-checkbox mt-checkbox-single mt-checkbox-outline">
@@ -50,13 +50,13 @@ module Erp
           <span></span>
       </label>').html_safe
     end
-    
+
     # Datalist pagination
     def erp_datalist_pagination(data)
       return '' if data.empty?
       render partial: "erp/helpers/list/datalist_pagination", locals: { data: data }
     end
-    
+
     # Get columns datalist
     def get_columns(params)
       params = params.to_unsafe_hash
@@ -70,44 +70,44 @@ module Erp
       end
       conds
     end
-    
+
     # Get custom uniquess id
     def unique_id
       return [*5..30000].sample.to_s
     end
-    
+
     # Format date
     def format_date(date)
       date.nil? ? '' : date.strftime(t('date_format'))
     end
-    
+
     # Format date
     def export_partial
       "erp/elements/export.xlsx.axlsx"
     end
-    
+
     def format_number(number, vn = false, round = false, precision = nil)
       prec = (number.to_f.round == number.to_f) ? 0 : 2
       prec = 0 if round
-      
+
       if !precision.nil?
         prec = precision
       end
-      
+
       if vn
         number_to_currency(number, precision: prec, separator: ",", unit: '', delimiter: ".")
       else
         number_to_currency(number, precision: prec, separator: ".", unit: '', delimiter: ",")
       end
     end
-    
-    
+
+
     @@mangso = ["không","một","hai","ba","bốn","năm","sáu","bảy","tám","chín"]
     def dochangchuc(so,daydu)
       chuoi = ""
       chuc = (so/10).to_i
       donvi = so%10
-      
+
       if chuc > 1
         chuoi = " " + @@mangso[chuc] + " mươi";
         if donvi == 1
@@ -121,7 +121,7 @@ module Erp
       elsif daydu && donvi>0
         chuoi = " lẻ"
       end
-      
+
       if donvi==5 && chuc>1
         chuoi += " lăm"
       elsif donvi>1 || ($donvi==1 && chuc==0)
@@ -129,7 +129,7 @@ module Erp
       end
       return chuoi
     end
-    
+
     def docblock(so,daydu)
       chuoi = ""
       tram = (so/100).floor
@@ -142,13 +142,13 @@ module Erp
       end
       return chuoi;
     end
-    
+
     def dochangtrieu(so,daydu)
       chuoi = ""
       trieu = (so/1000000).floor
       so = so%1000000
       if trieu>0
-        
+
         chuoi = docblock(trieu,daydu) + " triệu"
         daydu = true
       end
@@ -163,9 +163,9 @@ module Erp
       end
       return chuoi
     end
-      
+
     def docso(so)
-      return @@mangso[0] if so==0 
+      return @@mangso[0] if so==0
       chuoi = ""
       hauto = ""
       begin
@@ -178,33 +178,33 @@ module Erp
         end
         hauto = " tỷ"
       end while so>0
-      
+
       chuoi = chuoi.strip.capitalize
       chuoi = (chuoi =~ /Triệu /) == 0 ? "Một " + chuoi : chuoi
-      
+
       return chuoi.strip.capitalize + " đồng"
     end
-    
+
     # Status /list page
     def status_label(status)
       status.present? ? "<span class=\'label label-sm label-#{status}\'>#{t('.' + status)}</span>".html_safe : ''
     end
-    
+
     # image src
     def image_src(image, size)
       !image.nil? ? image.image_url.send(size).url : url_for("/backend/img/placeholder.jpg")
     end
-    
+
     # remove html and reuncate text
-    def short_text(text, length=50)      
+    def short_text(text, length=50)
       strip_tags(text).split[0..length-1].join(" ")
     end
-    
+
     # convert string to url friendly string
     def url_friendly(string)
       string.to_ascii.downcase.to_s.gsub(/[^0-9a-z ]/i, '').gsub(/ +/i, '-').strip
     end
-    
+
     # relative time
     def relative_time(start_time)
 			diff_seconds = (Time.now - start_time).to_i
@@ -215,7 +215,7 @@ module Erp
 					"#{diff_seconds/60} phút trước"
 				when 3600 .. (3600*24-1)
 					"#{diff_seconds/3600} giờ trước"
-				when (3600*24) .. (3600*24*30-1) 
+				when (3600*24) .. (3600*24*30-1)
 					"#{diff_seconds/(3600*24)} ngày trước"
 				when (3600*24*30) .. (3600*24*30*12-1)
 					"#{diff_seconds/(3600*24*30)} tháng trước"
@@ -223,11 +223,11 @@ module Erp
 					"#{diff_seconds/(3600*24*30*12)} năm trước"
 			end
 		end
-    
+
     # format price
     def format_price(price)
       number_to_currency(price, precision: 0, format: "%n đ", separator: ',')
     end
-    
+
   end
 end
--- public/backend/js/datalist.js
+++ public/backend/js/datalist.js
@@ -2,18 +2,18 @@
 function removeFromSelects(list, id) {
     var new_selects = [];
     var listid = list.attr('data-id');
-    
+
     // check if entry exist keyword groups
     selects[listid].forEach(function(entry) {
         var exist = false;
-        
+
         // check if entry exist keyword group entries
-        entry.forEach(function(entry2) {            
+        entry.forEach(function(entry2) {
             if(entry2.id == id) {
                 exist = true;
             }
         });
-        
+
         if(!exist) {
             new_selects.push(entry);
         }
@@ -24,15 +24,15 @@ function removeFromSelects(list, id) {
 // Add chose search entry to selects
 function addToSelects(list, item) {
     var id = list.attr('data-id');
-    
+
     // get item values
     var name = item.attr("name");
     var text = item.html();
     var value = item.attr('value');
     var label = item.parents('li').find('.keyword').attr('text');
-    
+
     var new_item = {id: guid(), name: name, text: text, value: value, label: label};
-    
+
     // Check if exists
     var exist = false;
     selects[id].forEach(function(entry, index, arrays) {
@@ -43,16 +43,16 @@ function addToSelects(list, item) {
             }
         });
     });
-    
+
     // add to keywords
     if(!exist) {
         selects[id].push([new_item]);
     }
-    
+
     console.log(selects[id]);
-    
+
     datalistFilter(list);
-    
+
     // clear current input
     list.find(".datalist-search-input").val("");
     list.find(".datalist-search-helper").hide();
@@ -62,12 +62,12 @@ function addToSelects(list, item) {
 function loadKeywordSelectList(container) {
     var list = container.parents('.datalist');
     var id = list.attr('data-id');
-    var url = container.attr('data-url');    
+    var url = container.attr('data-url');
     var keyword = list.find(".datalist-search-input").val();
-    var name = container.attr('name');    
-    
+    var name = container.attr('name');
+
     container.html('');
-    
+
     // ajax update custom sort
 	if(keywords_xhrs[id] && keywords_xhrs[id].readyState != 4){
 		keywords_xhrs[id].abort();
@@ -85,7 +85,7 @@ function loadKeywordSelectList(container) {
                 '</a></li>';
             container.append(html);
         });
-        
+
         // empty
         if(!result.length) {
             var html = '<li><a href="javascript:;">' +
@@ -105,12 +105,12 @@ function listActionProccess(link) {
     if(typeof(method) === 'undefined' || method.trim() === '') {
         method = 'GET';
     }
-    
+
     var url = link.attr("href");
     var list = link.parents(".datalist");
     var link_item = link;
-    var ids = getDataListCheckedIds(list); 
-    
+    var ids = getDataListCheckedIds(list);
+
     if(!ids.length) {
         swal({
             title: LANG_PLEASE_SELECT_AT_LEAST_ONE_ROW,
@@ -121,7 +121,7 @@ function listActionProccess(link) {
         });
         return;
     }
-    
+
     $.ajax({
         url: url,
         method: method,
@@ -138,7 +138,7 @@ function listActionProccess(link) {
             allowOutsideClick: true,
             confirmButtonText: "OK"
         });
-        
+
         // find outer datalist if exists
         if(link_item.parents('.datalist').length) {
             datalistFilter(link_item.parents('.datalist'));
@@ -151,7 +151,7 @@ function getDataListCheckedIds(list) {
     var ids = list.find("td input[name='ids[]']:checked").map(
         function () { return this.value; }
     ).get();
-    
+
     return ids;
 }
 
@@ -161,7 +161,7 @@ function checkDatalistCheckAllState(list) {
     var ids = getDataListCheckedIds(list);
     var action_button = list.find('.datalist-list-action');
     var row_count = list.find("td input[name='ids[]']").length;
-    
+
     // check length
     if(ids.length) {
         check_all_box.prop('checked', true);
@@ -170,7 +170,7 @@ function checkDatalistCheckAllState(list) {
         check_all_box.prop('checked', false);
         action_button.hide();
     }
-    
+
     // half check class
     if(ids.length < row_count) {
         check_all_box.parent().find('span').addClass('check-half');
@@ -183,18 +183,18 @@ function checkDatalistCheckAllState(list) {
 function removeFromKeywords(list, id) {
     var new_keywords = [];
     var listid = list.attr('data-id');
-    
+
     // check if entry exist keyword groups
     keywords[listid].forEach(function(entry) {
         var exist = false;
-        
+
         // check if entry exist keyword group entries
-        entry.forEach(function(entry2) {            
+        entry.forEach(function(entry2) {
             if(entry2.id == id) {
                 exist = true;
             }
         });
-        
+
         if(!exist) {
             new_keywords.push(entry);
         }
@@ -205,14 +205,14 @@ function removeFromKeywords(list, id) {
 // removeDatalistSearchItem
 function removeDatalistSearchItem(list, id) {
     var item = $('[data-id="' + id + '"]');
-    
+
     // if is checkable list
     if(item.length) {
         checkCheckableItem($('[data-id="' + id + '"]'));
     } else {
         // if it is keyword
         removeFromKeywords(list, id);
-        
+
         // if it is selected item
         removeFromSelects(list, id);
     }
@@ -221,19 +221,19 @@ function removeDatalistSearchItem(list, id) {
 // Add to keywords
 function addToKeywords(list, item) {
     var id = list.attr('data-id');
-    
+
     if(typeof(item) == 'undefined') {
         // get default keyword
-        item = list.find(".datalist-search-helper ul li").eq(0).find("a .keyword");        
+        item = list.find(".datalist-search-helper ul li").eq(0).find("a .keyword");
     }
-    
+
     // get item values
     var name = item.attr("name");
     var text = item.attr("text");
     var value = item.html();
-    
-    item = {id: guid(), name: name, text: '<span class="ktext">' + value + '</span>', value: value, label: text};
-    
+
+    item = {id: guid(), name: name, text: value, value: value, label: text};
+
     // Check if exists
     var exist = false;
     keywords[id].forEach(function(entry) {
@@ -248,20 +248,20 @@ function addToKeywords(list, item) {
                 inserted = true;
             }
         });
-        
+
         if(!inserted) {
             entry.push(item);
         }
     });
-    
+
     // add to keywords
     if(!exist) {
         keywords[id].push([item]);
     }
-    
+
     // console.log(keywords[id]);
     datalistFilter(list);
-    
+
     // clear current input
     list.find(".datalist-search-input").val("");
     list.find(".datalist-search-helper").hide();
@@ -271,14 +271,14 @@ function addToKeywords(list, item) {
 function toggleDatalistSearchHelper(list) {
     var keyword = list.find(".datalist-search-input").val();
     var helper = list.find(".datalist-search-helper");
-    
+
     // Toggle helper box
     if(keyword.trim() !== '') {
         helper.show();
     } else {
         helper.hide();
     }
-    
+
     // Update keyword field
     helper.find('.keyword').html(keyword);
 }
@@ -313,17 +313,17 @@ function addItemToListSearch(list, ors, label) {
     var html = '';
     var texts = [];
     var ids = [];
-    
+
     ors.forEach(function(entry) {
         texts.push(entry.text);
         ids.push(entry.id);
-        
+
         // overide label
         if(entry.label) {
             label = entry.label;
         }
     });
-    
+
     html = '<div class="btn-group btn-group-solid list-search-item" data-ids="' + ids.join(',') + '">' +
         '<button type="button" class="btn green-meadow">' +
             label +
@@ -333,7 +333,7 @@ function addItemToListSearch(list, ors, label) {
         '</button>' +
         '<button type="button" class="btn grey cancel-button"><i class="fa fa-close"></i></button>' +
     '</div>';
-    
+
     box.append(html);
 }
 
@@ -354,7 +354,7 @@ function getValuesFromCheckableList(items) {
         var value = $(this).attr('value');
         var text = $(this).find('a').html();
         var checked = $(this).hasClass('checked');
-        
+
         // Push conditions to ors array
         if(typeof(name) !== 'undefined' && name.trim() !== '') {
             if(checked) {
@@ -364,7 +364,7 @@ function getValuesFromCheckableList(items) {
                 ors.push({'id': id, 'name': name, 'value': value, 'text': text.trim()});
             }
         }
-        
+
         // Check if end of ors
         if($(this).hasClass('divider') && ors.length) {
             conditions.push(ors);
@@ -375,7 +375,7 @@ function getValuesFromCheckableList(items) {
     if(ors.length) {
         conditions.push(ors);
     }
-    
+
     return conditions;
 }
 
@@ -391,7 +391,7 @@ function updateDatalistSortLayout(list) {
     var sort_by = list.attr('sort-by');
     var sort_direction = list.attr('sort-direction');
     var th = list.find('th.sortable[sort-by="' + sort_by + '"]');
-    
+
     th.addClass('current');
     th.attr('sort-direction', sort_direction);
 }
@@ -402,25 +402,25 @@ function datalistFilter(list, page) {
     var id = list.attr('data-id');
     var sort_by = list.attr('sort-by');
     var sort_direction = list.attr('sort-direction');
-    
+
     // check page
     if(typeof(page) == 'undefined') {
         page = 1;
     }
-    
+
     // save current page
     list.attr('data-page', page);
-    
+
     // Clear list search
     list.find('.list-search-items').html('');
-    
+
     // Filters
-    var filters = getValuesFromCheckableList(list.find('.datalist-filters li'));    
+    var filters = getValuesFromCheckableList(list.find('.datalist-filters li'));
     addItemsToListSearch(list, filters, '<i class="fa fa-filter"></i>');
-    
+
     // Columns
     var columns = getValuesFromCheckableList(list.find('.datalist-columns-select li'));
-    
+
     // Keywords
     if(typeof(keywords[id]) == 'undefined') {
         keywords[id] = [];
@@ -428,7 +428,7 @@ function datalistFilter(list, page) {
     keywords[id].forEach(function(entry) {
         addItemToListSearch(list, entry, entry[0].label);
     });
-    
+
     // Selects
     if(typeof(selects[id]) == 'undefined') {
         selects[id] = [];
@@ -437,7 +437,7 @@ function datalistFilter(list, page) {
         addItemToListSearch(list, entry, entry[0].label);
     });
     filters = filters.concat(selects[id]);
-    
+
     // ajax update custom sort
 	if(datalists[id] && datalists[id].readyState != 4){
 		datalists[id].abort();
@@ -456,13 +456,13 @@ function datalistFilter(list, page) {
         },
     }).done(function( html ) {
         list.find(".datalist-container" ).html( html );
-        
+
         // update list sort layout
         updateDatalistSortLayout(list);
-        
+
         // hide actions button
         checkDatalistCheckAllState(list);
-        
+
         // Tooltip
         jsForAjaxContent(list);
     });
@@ -476,150 +476,150 @@ var selects = {};
 $(document).ready(function() {
     // Filter all datalists in first load
     datalistFilterAll();
-    
-    // Filters group link click    
+
+    // Filters group link click
     $(document).on('click', '.btn-group-checkable .dropdown-menu>li>a', function(e) {
         e.preventDefault();
         e.stopPropagation();
-        
+
         var li = $(this).parents('li');
-        
+
         checkCheckableItem(li);
     });
-    
+
     // Filter when click checkable items
     $(document).on('click', '.btn-group-checkable .dropdown-menu>li>a', function() {
         var list = $(this).parents('.datalist');
         datalistFilter(list);
     });
-    
+
     // Change page
     $(document).on("click", ".datalist .pagination a", function(e) {
         var url = $(this).attr("href");
         var list = $(this).parents('.datalist');
         var page = getUrlParameter(url, 'page');
-        
+
         datalistFilter(list, page);
 
         e.preventDefault();
         e.stopPropagation();
         e.stopImmediatePropagation();
     });
-    
+
     // Remove item from search list
     $(document).on("click", ".list-search-item .cancel-button", function() {
         var item = $(this).parents('.list-search-item');
         var ids = item.attr('data-ids');
         var list = $(this).parents('.datalist');
-        
+
         ids.split(",").forEach(function(id) {
             // checkCheckableItem($('[data-id="' + id + '"]'));
             removeDatalistSearchItem(list, id);
         });
-        
+
         datalistFilter(list);
     });
-    
+
     // Datalist search input
     $(document).on("focus", ".datalist-search-input", function() {
         var list = $(this).parents('.datalist');
         toggleDatalistSearchHelper(list);
     });
-    
+
     // Datalist search input
     $(document).on("keyup", ".datalist-search-input", function(e) {
         var list = $(this).parents('.datalist');
-        toggleDatalistSearchHelper(list);        
-        
+        toggleDatalistSearchHelper(list);
+
         // show keyword-select if exits
         list.find('.keyword-select').each(function () {
             loadKeywordSelectList($(this));
         });
-        
+
         var code = e.which;
         if(code==13) e.preventDefault();
         if(code==13){
             // add keyword to list
             addToKeywords(list);
         }
-        
+
     });
-    
+
     // Datalist search helper click
     $(document).on("click", ".datalist-search-helper ul li a.keyword-line", function() {
         var list = $(this).parents('.datalist');
         var keyword = $(this).find(".keyword");
-        
+
         addToKeywords(list, keyword);
     });
-    
+
     // Hide datalist helper
     $(document).mouseup(function (e)
     {
         var container = $(".list-filters-search");
         var helper = $(".datalist-search-helper");
-        
+
         // if the target of the click isn't the container...
         if (!container.is(e.target) && container.has(e.target).length === 0) // ... nor a descendant of the container
         {
             helper.hide();
         }
     });
-    
+
     // Check all list click
     $(document).on('click', '.datalist-checkbox-all', function() {
         var list = $(this).parents('.datalist');
-        var item = $(this);        
+        var item = $(this);
         var checked = item.is(':checked');
-        var action_button = list.find('.datalist-list-action');       
-        
+        var action_button = list.find('.datalist-list-action');
+
         // check all
         if(checked) {
             list.find('.datalist-row-checkbox').prop('checked', true);
         } else {
             list.find('.datalist-row-checkbox').prop('checked', false);
         }
-        
+
         checkDatalistCheckAllState(list);
     });
-    
+
     // Check row checkbox
     $(document).on('click', '.datalist-row-checkbox', function() {
         var list = $(this).parents('.datalist');
-        
+
         checkDatalistCheckAllState(list);
     });
-    
+
     // List action
     // Datalist action link with message return
     $(document).on('click', '.datalist-list-action ul li a', function(e) {
         e.preventDefault();
-        
+
         listActionProccess($(this));
     });
-    
+
     // Datalist action link with message return
     $(document).on('click', '.datalist-search-helper ul li .keyword-select-pointer', function() {
         var select = $(this).parents('li').find('ul');
         select.toggle();
-        
+
         if(select.is(':visible')) {
             $(this).addClass('open');
         } else {
             $(this).removeClass('open');
         }
     });
-    
+
     // Sort head click event
     $(document).on('click', '.datalist th.sortable', function() {
         var list = $(this).parents('.datalist');
         var is_current = $(this).hasClass('current');
         var sort_direction = $(this).attr('sort-direction');
-        
+
         if(typeof(sort_direction) === 'undefined' || sort_direction.trim() === '') {
             sort_direction = 'asc';
         }
-        
+
         if(is_current) {
             if (sort_direction == 'asc') {
                 sort_direction = 'desc';
@@ -632,15 +632,15 @@ $(document).ready(function() {
             list.attr('sort-by', $(this).attr('sort-by'));
             list.attr('sort-direction', sort_direction);
         }
-        
+
         datalistFilter(list);
     });
-    
+
     // Datalist search entry select
     $(document).on("click", ".datalist-search-helper ul li a.keyword-select-item", function() {
         var list = $(this).parents('.datalist');
         var item = $(this);
-        
+
         addToSelects(list, item);
     });
 });
