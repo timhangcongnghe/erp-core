07b38202adc49d0d03b25c22e75a089d39f854a5
sonnguyenngoc <sonnn@hoangkhang.com.vn>
2017-04-12 00:58:12 -0700

    reback

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
 # Create default admin user for developing
 puts "Create default admin user"
-Erp::User.create(email: "admin@phoviptour.vn", password: "aA456321@", name: "Phố Vip Tour", backend_access: true) if Erp::User.where(email: "admin@phoviptour.vn").empty?
+Erp::User.create(email: "admin@globalnaturesoft.com", password: "aA456321@", name: "Super Admin", backend_access: true) if Erp::User.where(email: "admin@globalnaturesoft.com").empty?
