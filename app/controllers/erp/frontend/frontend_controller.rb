f34ca30a5e4abc2270e52d6c760ddaeb9fa541ed
Luan Pham <luanpm88@gmail.com>
2017-04-17 19:56:16 -0700

    fix helper

--- app/controllers/erp/frontend/frontend_controller.rb
+++ app/controllers/erp/frontend/frontend_controller.rb
@@ -2,14 +2,15 @@ require_dependency "erp/application_controller"
 
 module Erp::Frontend
   class FrontendController < Erp::ApplicationController
+		include Erp::ApplicationHelper
     before_filter :set_view
-    
+
     layout 'erp/frontend/index'
-    
+
     def current_ability
 			@current_ability ||= Erp::Ability.new(current_user)
 		end
-    
+
     private
 			def set_view
 				session[:current_view] = "frontend"
