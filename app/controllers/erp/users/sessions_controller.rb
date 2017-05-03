cadbb8715e5fbd69afe48f20eaa46c4899c31f8c
Luan Pham <luanpm88@gmail.com>
2017-05-02 21:24:36 -0700

    format code

--- app/controllers/erp/users/sessions_controller.rb
+++ app/controllers/erp/users/sessions_controller.rb
@@ -1,18 +1,18 @@
 module Erp
 	class Users::SessionsController < Devise::SessionsController
 		layout :set_layout
-		
+
 		def new
 			self.resource = resource_class.new(sign_in_params)
 			clean_up_passwords(resource)
 			yield resource if block_given?
 			respond_with(resource, serialize_options(resource))
 		end
-		
+
 		private
 			def set_layout
 				return nil if request.xhr?
-				
+
 				if session[:current_view] == 'frontend'
 					'erp/frontend/index'
 				else
