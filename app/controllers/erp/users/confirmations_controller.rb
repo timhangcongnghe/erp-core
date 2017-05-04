10a08fbe517c3acdd13eaad48af2fff6b3d3a3ce
Luan Pham <luanpm88@gmail.com>
2017-05-04 00:26:20 -0700

    confirmation link click more than once

--- app/controllers/erp/users/confirmations_controller.rb
+++ app/controllers/erp/users/confirmations_controller.rb
@@ -2,6 +2,20 @@ module Erp
   class Users::ConfirmationsController < Devise::ConfirmationsController
     layout :set_layout
 
+    # GET /resource/confirmation?confirmation_token=abcdef
+    def show
+      self.resource = resource_class.confirm_by_token(params[:confirmation_token])
+      yield resource if block_given?
+
+      if resource.errors.empty?
+        set_flash_message!(:notice, :confirmed)
+        respond_with_navigational(resource){ redirect_to after_confirmation_path_for(resource_name, resource) }
+      else
+        flash[:notice] = "Your account is already confirmed. Please login."
+        redirect_to '/#sign-in'
+      end
+    end
+
     private
       def after_confirmation_path_for(resource_name, resource)
         '/'
