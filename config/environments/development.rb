08084d36216ca393e9f876b42190d1b9fcd7be64
Luan Pham <luanpm88@gmail.com>
2017-05-23 01:27:05 -0700

    change production domain

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,7 +52,7 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_mailer.default_url_options = { :host => 'dev.timhangcongnghe.vn' }
+  config.action_mailer.default_url_options = { :host => 'timhangcongnghe.vn' }
 
   # set delivery method to :smtp, :sendmail or :test
   config.action_mailer.delivery_method = :smtp
