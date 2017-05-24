ebee18d3cc350afcebc6c794edd78cdfa551e8a4
Luan Pham <luanpm88@gmail.com>
2017-05-23 19:05:08 -0700

    font color editor

--- public/backend/js/editor.js
+++ public/backend/js/editor.js
@@ -5,11 +5,11 @@ $(document).ready(function() {
       menubar: true,
       relative_urls: false,
       plugins: [
-        'table advlist autolink lists link image charmap print preview anchor',
+        'textcolor table advlist autolink lists link image charmap print preview anchor',
         'searchreplace visualblocks code fullscreen',
         'insertdatetime media table contextmenu paste code'
       ],
-      toolbar: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+      toolbar: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | forecolor backcolor | bullist numlist outdent indent | link image',
       content_css: '//www.tinymce.com/css/codepen.min.css'
     });
 });
