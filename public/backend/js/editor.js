f063980557ec1ddd4cfda8f09445f8d149e3e65e
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-06-19 23:46:05 -0700

    add source code button to tiny editor

--- public/backend/js/editor.js
+++ public/backend/js/editor.js
@@ -24,7 +24,7 @@ $(document).ready(function() {
         'searchreplace visualblocks code fullscreen',
         'insertdatetime media table contextmenu paste code'
       ],
-      toolbar: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+      toolbar: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | code',
       content_css: '//www.tinymce.com/css/codepen.min.css',
       statusbar: false,
     });
