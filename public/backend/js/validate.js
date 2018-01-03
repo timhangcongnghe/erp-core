ce8f4e80fbb506eb49e78b8edec80fd56fd1fe03
Hung Nguyen <hungnt@hoangkhang.com.vn>
2018-01-03 22:41:24 +0700

    fix bug: check validate for form

--- public/backend/js/validate.js
+++ public/backend/js/validate.js
@@ -1,61 +1,64 @@
 function customValidate(container) {
-    //Initialize the validation object which will be called on form submit.
-    var validobj = container.find('form').validate({
-        onkeyup: false,
-        errorClass: "myErrorClass",
-        ignore: ".jvalidate_ignore",
-
-        //put error message behind each form element
-        errorPlacement: function (error, element) {
-            var elem = $(element);
-            error.insertAfter(element);
-        },
-
-        //When there is an error normally you just add the class to the element.
-        // But in the case of select2s you must add it to a UL to make it visible.
-        // The select element, which would otherwise get the class, is hidden from
-        // view.
-        highlight: function (element, errorClass, validClass) {
-            var elem = $(element);
-            if (elem.hasClass("select2-offscreen")) {
-                $("#s2id_" + elem.attr("id") + " ul").addClass(errorClass);
-            } else {
-                elem.addClass(errorClass);
+    container.find('form').each(function(){
+        //Initialize the validation object which will be called on form submit.
+        var validobj = $(this).validate({
+            onkeyup: false,
+            errorClass: "myErrorClass",
+            ignore: ".jvalidate_ignore",
+    
+            //put error message behind each form element
+            errorPlacement: function (error, element) {
+                var elem = $(element);
+                error.insertAfter(element);
+            },
+    
+            //When there is an error normally you just add the class to the element.
+            // But in the case of select2s you must add it to a UL to make it visible.
+            // The select element, which would otherwise get the class, is hidden from
+            // view.
+            highlight: function (element, errorClass, validClass) {
+                var elem = $(element);
+                if (elem.hasClass("select2-offscreen")) {
+                    $("#s2id_" + elem.attr("id") + " ul").addClass(errorClass);
+                } else {
+                    elem.addClass(errorClass);
+                }
+            },
+    
+            //When removing make the same adjustments as when adding
+            unhighlight: function (element, errorClass, validClass) {
+                var elem = $(element);
+                if (elem.hasClass("select2-offscreen")) {
+                    $("#s2id_" + elem.attr("id") + " ul").removeClass(errorClass);
+                } else {
+                    elem.removeClass(errorClass);
+                }
             }
-        },
-
-        //When removing make the same adjustments as when adding
-        unhighlight: function (element, errorClass, validClass) {
-            var elem = $(element);
-            if (elem.hasClass("select2-offscreen")) {
-                $("#s2id_" + elem.attr("id") + " ul").removeClass(errorClass);
-            } else {
-                elem.removeClass(errorClass);
+        });
+    
+        //If the change event fires we want to see if the form validates.
+        //But we don't want to check before the form has been submitted by the user
+        //initially.
+        $(document).on("change", ".select2-offscreen", function () {
+            if (!$.isEmptyObject(validobj.submitted)) {
+                validobj.form();
             }
-        }
+        });
+    
+        //A select2 visually resembles a textbox and a dropdown.  A textbox when
+        //unselected (or searching) and a dropdown when selecting. This code makes
+        //the dropdown portion reflect an error if the textbox portion has the
+        $(document).on("select2-opening", function (arg) {
+            var elem = $(arg.target);
+            if ($("#s2id_" + elem.attr("id") + " ul").hasClass("myErrorClass")) {
+                //jquery checks if the class exists before adding.
+                $(".select2-drop ul").addClass("myErrorClass");
+            } else {
+                $(".select2-drop ul").removeClass("myErrorClass");
+            }
+        });
     });
-
-    //If the change event fires we want to see if the form validates.
-    //But we don't want to check before the form has been submitted by the user
-    //initially.
-    $(document).on("change", ".select2-offscreen", function () {
-        if (!$.isEmptyObject(validobj.submitted)) {
-            validobj.form();
-        }
-    });
-
-    //A select2 visually resembles a textbox and a dropdown.  A textbox when
-    //unselected (or searching) and a dropdown when selecting. This code makes
-    //the dropdown portion reflect an error if the textbox portion has the
-    $(document).on("select2-opening", function (arg) {
-        var elem = $(arg.target);
-        if ($("#s2id_" + elem.attr("id") + " ul").hasClass("myErrorClass")) {
-            //jquery checks if the class exists before adding.
-            $(".select2-drop ul").addClass("myErrorClass");
-        } else {
-            $(".select2-drop ul").removeClass("myErrorClass");
-        }
-    });
+        
 }
 
 $(document).ready(function () {
