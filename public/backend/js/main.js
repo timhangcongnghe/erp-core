c089cb8c9639204e2706d7fb3f6e641fe8da24f2
Ton Hung Nguyen <tonhungstar@gmail.com>
2018-11-01 07:22:28 +0000

    fix bug js on click button

--- public/backend/js/main.js
+++ public/backend/js/main.js
@@ -1094,16 +1094,18 @@ $(document).ready(function() {
         submitModalHasForm($(this));
     });
     
-    $(document).on("click", ".one_click_btn", function(e) {
-        //var label_html = $(this).html();
-        //var label_val = $(this).val();
-            
+    $(document).on("click", ".one_click_btn", function(e) {            
         if ($(this).closest('form').valid()) {
-            $(this).html('Đang xử lý... Hãy chờ!');
-            $(this).val('Đang xử lý... Hãy chờ!');
-            $(this).addClass('btn-disabled');
-            $(this).closest('form').find(":submit").prop("disabled", true);
-            $(this).closest('form').find(":submit").addClass('btn-disabled');
+            if ($(this).prop('tagName').toLowerCase() == 'button') {
+                $(this).html('Đang xử lý... Hãy chờ!');
+            }
+            
+            if ($(this).prop('tagName').toLowerCase() == 'input') {
+                $(this).val('Đang xử lý... Hãy chờ!');
+            }
+            
+            $(this).addClass('btn-disabled'); // them class vao nut hien tai
+            $(this).closest('form').find(":submit").addClass('btn-disabled'); // them class vao nut khac
             $(this).closest('form').submit();
         }
     });
