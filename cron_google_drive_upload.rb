057a58c059ad1fe4db49cb939896c3abea67bcbe
Son Nguyen <sonnn@hoangkhang.com.vn>
2019-11-15 09:54:50 +0700

    update server core

--- cron_google_drive_upload.rb
+++ cron_google_drive_upload.rb
@@ -6,8 +6,8 @@ DIR = File.expand_path(File.dirname(__FILE__))
 require DIR+'/app/models/erp/system.rb'
 
 Erp::System.upload_backup_to_google_drive({
-  backup_dir: DIR+'/../../backup',
+  backup_dir: DIR+'/../../../backup',
   dir: DIR+'/../../',
   token: DIR+'/../../cron_google_drive_token.conf',
-  revision_max: 20
+  revision_max: 2
 })
