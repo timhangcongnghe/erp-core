5a45d0fdda1263b7b5071858cbe01b24895f492a
Nguyen Ngoc Son <sonnn0811@icloud.com>
2021-01-19 09:45:58 +0700

    update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,7 +22,6 @@ Rails.application.configure do
     }
   else
     config.action_controller.perform_caching = false
-
     config.cache_store = :null_store
   end
 
@@ -40,7 +39,7 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  # config.assets.debug = true
+  config.assets.debug = false
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
