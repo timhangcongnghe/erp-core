f7d0ffe3128c1088fac97b0d86f82ca037a36ed9
Nguyen Ngoc Son <sonnn0811@icloud.com>
2021-01-26 09:32:43 +0700

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 # Create default admin user for developing
 puts "Create default admin user"
 Erp::User.create(
-  email: "admin@globalnaturesoft.com",
+  email: "admin@phatgiaodinhquan.org",
   password: "aA456321@",
   name: "Super Admin",
   backend_access: true,
   confirmed_at: Time.now-1.day,
   active: true
-) if Erp::User.where(email: "admin@globalnaturesoft.com").empty?
+) if Erp::User.where(email: "admin@phatgiaodinhquan.org").empty?
